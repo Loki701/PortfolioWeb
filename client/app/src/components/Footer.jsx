8de8c1b7b6780aca209abd2da2e2779b942f8ce6
Loki701 <86447514+Loki701@users.noreply.github.com>
2024-03-19 18:35:44 -0400

    Update Footer.jsx

--- client/app/src/components/Footer.jsx
+++ client/app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () =>{
     return(
         <footer className="footer">
         <div className="footer-text">
-          <p>Copyright &copy; 2023 by Jose Figueredo | All Rights Reserved.</p>
+          <p>Copyright &copy; 2024 by Jose Figueredo | All Rights Reserved.</p>
         </div>
         <div className="footer-iconTop">
 
